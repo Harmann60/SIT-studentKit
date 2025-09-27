61f8961921d4051263f3ebdea0a232bbe78c722e
Harmann60 <harmanjss10@gmail.com>
2025-09-27 14:35:32 +0530

    Updated homepage design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ const Home = () => {
     return (
         <div>
             <h1>Welcome to SIT Student Kit</h1>
-            <p>This is the Home page.</p>
+            <h2>Life at SIT Pune </h2>
+             <p>Symbiosis Institute of Technology (SIT), Pune offers a vibrant learning environment that blends academic excellence with holistic development. With state-of-the-art infrastructure, modern laboratories, and well-equipped classrooms, SIT ensures students gain both theoretical knowledge and practical skills. The campus fosters innovation through research, industry collaborations, and project-based learning, while a wide range of cultural, technical, and sports activities enrich student life. Surrounded by the scenic Lavale hills, SIT provides a perfect balance of focused study and campus vibrancy, preparing students to excel as global professionals and responsible leaders.</p>
+            <h3> Academics</h3>
         </div>
     );
 };
